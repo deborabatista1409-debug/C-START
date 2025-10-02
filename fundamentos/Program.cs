c314ab61aedd3d0297c1f573740f48535da5eaf6
Debbys <deborabatista1409@gmail.com>
2025-10-01 20:54:12 -0400

    variavel

---
+++ fundamentos/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region BIBLIA 
+Console.WriteLine("Hello, World!");
+
+
+#endregion
+#region Tipos de váriaveis
+int idade = 22; //numero inteiros
+
+// numeros quebrados
+double peso = 47.7;
+
+// manipulação de string  textos
+
+string Nome001;
+
+// tipo boleano (verdadeiro ou falso)
+bool estudante = true;
+bool estudante001 = false;
+bool estudante002 = true;
+
+#endregion
+
+#region 
+string Nome = "DADOS";
+
+Console.WriteLine($"ANALISE DE {Nome}");
+
+#endregion
+
+#region 
+// inserir dados
+
+Console.WriteLine("Digite seu nome:");
+string nomeInput = Console.ReadLine()!;
+Console.WriteLine("Digite seu curso:");
+string CursoInput = Console.ReadLine()!;
+Console.WriteLine($"{nomeInput} seu CERTIFICADO DE {CursoInput} estará DISPONÍVEL EM 14 DIAS");
+
+
+#endregion
