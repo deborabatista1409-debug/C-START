08fb0559e0b85b5dd910c2fb765db2387e8ea59f
DEBORUS <deborabatista1409@gmail.com>
2025-10-13 20:21:25 -0400

    atividade 1

--- fundamentos/Program.cs
+++ fundamentos/Program.cs
@@ -1,10 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-#region BIBLIA 
-Console.WriteLine("Hello, World!");
-
-
-#endregion
-#region Tipos de váriaveis
+﻿//#region BIBLIA 
+//Console.WriteLine("Hello, World!");
+//#endregion
+//#region Tipos de váriaveis
+/*
 int idade = 22; //numero inteiros
 
 // numeros quebrados
@@ -18,24 +16,97 @@ string Nome001;
 bool estudante = true;
 bool estudante001 = false;
 bool estudante002 = true;
+*/
+//#endregion
+//#region 
+// string Nome = "DADOS";
 
-#endregion
+// Console.WriteLine($"ANALISE DE {Nome}");
 
-#region 
-string Nome = "DADOS";
+//#endregion
 
-Console.WriteLine($"ANALISE DE {Nome}");
+/*#region Q 01 - Somar dois número
 
-#endregion
+using System.Globalization;
 
-#region 
-// inserir dados
+int primeiroNumero, segundoNumero, soma;
+
+// Entrada
+Console.WriteLine("Digite seu nome:");
+primeiroNumero = int.Parse(Console.ReadLine()!);
 
 Console.WriteLine("Digite seu nome:");
-string nomeInput = Console.ReadLine()!;
-Console.WriteLine("Digite seu curso:");
-string CursoInput = Console.ReadLine()!;
-Console.WriteLine($"{nomeInput} seu CERTIFICADO DE {CursoInput} estará DISPONÍVEL EM 14 DIAS");
+segundoNumero = int.Parse(Console.ReadLine()!);
+
+//Processamento
+
+soma = primeiroNumero + segundoNumero;
+
+//Saida
+
+Console.WriteLine($"O valor digitado é {soma}");
+
+#endregion questão 2
+*/
+/*
+#region atividade 2
+
+float num1;
+
+double num2;
 
 
+Console.Write(" Digite um numero float: ");
+num1 = float.Parse(Console.ReadLine()!);
+
+Console.Write(" Digite um numero double: ");
+num2 = double.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine($"A soma de dois numero {num1 + num2} ");
+
+#endregion
+/*
+
+*/
+
+#region   questão 3
+//REFAZER
+
+
+#endregion
+
+#region Q4 - Print name✅
+/*
+string  nome;
+
+Console.Write(" Digite um nome ");
+nome = Console.ReadLine();
+
+Console.WriteLine($" Olá, {nome} seja bem-vindo");
+*/
+
+#endregion
+
+
+#region Q5 Multiplicaçaõ✅
+/*
+float num3;
+float num4, multi;
+
+Console.WriteLine(" Digite um numero decimal");
+num3 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Digite outro numero decimal:");
+num4 = float.Parse(Console.ReadLine()!);
+
+multi = num3 * num4;
+
+Console.WriteLine($"A multiplicação de {num3} com {num4} é {multi:F2}");
+
+*/
+
+#endregion 
+
+#region 06
 #endregion
