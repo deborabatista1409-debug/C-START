c9e3bf6821db3f9b6e6be62d632c6baad3ad1912
DEBORUS <deborabatista1409@gmail.com>
2025-10-17 21:01:31 -0400

    IF E ELSE - ESTRUTURAS CONDICIONAIS

---
+++ EstruturaDeControle/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Numero
+/*
+var numero = -10;
+Console.WriteLine($" Vamos Verficar se o numero {numero} é positivo ou negativo");
+
+if (numero > 0)
+{
+    Console.WriteLine("Esse numero é positivo");
+}
+ else
+{
+    Console.WriteLine("Esse numero é negativo");
+}
+
+*/
+#endregion
+
+#region CHUVISCO
+/*
+bool chuva = false;
+
+if (chuva)
+
+{
+
+    Console.WriteLine("Esta Chovendo");
+}
+
+else
+
+{
+
+    Console.WriteLine("Não esta chovendo, continue o passeio");
+}
+
+*/
+
+
+#endregion
+
+
+#region 
